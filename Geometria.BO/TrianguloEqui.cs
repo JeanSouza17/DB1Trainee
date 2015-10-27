df58110808032db3cca659d4249d4ad58d830034
JeanSouza17 <jeancontrigiani@gmail.com>
2015-10-27 14:26:28 -0200

    DB1 Trainee

---
+++ Geometria.BO/TrianguloEqui.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria.BO
+{
+    public class TrianguloEqui : Formas
+    {
+        public decimal Perimetro(decimal valor) 
+        {
+            return valor * 3;
+        }
+        public decimal Area(decimal valor1) 
+        {
+            return (valor1 * valor1) / 2;
+        }
+        
+    }
+}
---
+++ Geometria.UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Geometria.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria.UI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Programa.Calculadora();
+            Console.ReadKey();
+        }
+    }
+}
